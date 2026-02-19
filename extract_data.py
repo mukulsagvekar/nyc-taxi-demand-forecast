0059c69ccbe3aabafef17859348646f741e1afaf
mukulsagvekar <mukul.sagvekar@gmail.com>
2026-02-19 12:42:53 -0600

    initial commit

---
+++ extract_data.py
@@ -0,0 +1,24 @@
+import requests
+import os
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+today = datetime.now()
+three_months_ago = today - relativedelta(months=1) # as almost 3 month lag
+
+year = three_months_ago.year
+month = three_months_ago.month
+
+filename = f"yellow_tripdata_{year}-{month}.parquet"
+url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{filename}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    if filename not in os.listdir("data"):
+        with open(f"data/{filename}", 'wb') as f:
+            f.write(response.content)
+    else:
+        print(f"{filename} already exists in the data directory.")
+else:
+    print(f"No data available for the specified {year}_{month}.")
